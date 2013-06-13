82a2ba78a273a4d289da5d38a186c404003b7690
moiyer <zbzhengbin1989@gmail.com>
2013-06-13 16:31:32 +0800

    题目不难，循环实现，下次用递归

--- Reverse Nodes in k-Group.cpp
+++ Reverse Nodes in k-Group.cpp
@@ -28,46 +28,44 @@ For k = 3, you should return: 3->2->1->4->5
  */
 
  //2013/6/10 22:40:22
- //每次将最后一个插入到头之前
+ //每次将这一段的最后一个插入到头之前,写了很久，开始用递归写，总有错误
 class Solution {
 public:
-	ListNode** recurssive(ListNode** pHead, int k, int* error)
-	{
-		if(k == 1)return &((*pHead)->next);
-
-		ListNode** pTail = pHead;
-		ListNode* tp;
-
-		for(int i = 2; i <= k; ++i)
-		{
-			if((*pTail)->next == NULL){
-				*error = -1;
-				return NULL;
-			}
-			pTail = &((*pTail)->next);
-		}
-
-		tp = *pTail;
-		*pTail = (*pTail)->next;
-		tp->next = (*pHead);
-		*pHead = tp;
-
-		return recurssive(&((*pHead)->next), k - 1, error);
-	}
 
     ListNode *reverseKGroup(ListNode *head, int k) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if(head == NULL || k <= 0)return NULL;
+        if(k < 2 || head == NULL)return head;
 
         ListNode **cur = &head;
-        ListNode **pp;
-        int error = 0;
+        int j = 1;
         while(1)
         {
-        	pp = recurssive(cur, k, &error);
-        	if(error == -1)break;
-        	cur = pp;
+        	ListNode** pTail = cur;
+        	ListNode** pHead = cur;
+        	ListNode* tp;
+        	for(j = k; j >= 2; --j)
+        	{
+        		for(int i = 2; i <= j; ++i)
+        		{
+        			if((*pTail)->next == NULL)
+        			{
+        				return head;
+        			}
+        			pTail = &((*pTail)->next);
+        		}
+
+        		tp = *pTail;
+        		*pTail = (*pTail)->next;
+        		tp->next = (*pHead);
+        		*pHead = tp;
+
+        		pHead = &(tp->next);
+        		pTail = pHead;
+        	}
+        	
+        	if((*pTail)->next == NULL)break;
+        	cur = &((*pTail)->next);
         }
 
         return head;

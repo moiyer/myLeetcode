6c10941330568c6d2870a537bd7f131a95dca883
moiyer <zbzhengbin1989@gmail.com>
2013-06-17 15:29:25 +0800

    别人的解法，没有自己写

--- Longest Valid Parentheses.cpp
+++ Longest Valid Parentheses.cpp
@@ -42,24 +42,65 @@ public:
     }
 };
 
-//2013/6/14 17:13:47
-//使用stack记录'('
-
+//2013/6/17 14:47:01
+//使用计数和模板实现
+//Find from left to right by a forward iterator and right to left by a reverse iterator. 
+//Using template to avoid duplicate code since a reverse iterator overloads the operator +.
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int maxLength = 0;
-        stack<int> left;
+        return max( helper(s.begin(), s.end(), '('), helper(s.rbegin(), s.rend(), ')') );
+    }
 
-        int size = s.length();
-
-        for(int i = 0; i < size; ++i)
-        {
-        	if(s[i] == '(')left.push(i);
+    template<class Iter>
+    int helper(Iter beg, Iter end, char ch) {
+        int ret = 0;
+        int left = 0, right = 0;
+        for ( ; beg != end; ++beg) {
+            if (*beg == ch) {
+                ++left;
+            }
+            else if (left > right) {
+                ++right;
+                if (left == right) {
+                    ret = max(ret, left<<1);
+                }
+            }
+            else {
+                left = right = 0;
+            }
         }
+        return ret;
+    }
+};
 
-        return maxLength << 1;
+//2013/6/17 14:56:01
+//这种方法比较主流
+//Use a stack to keep track of the positions of non-matching '('s. 
+//Also need to keep track of the position of the last ')'.
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int maxLen = 0, last = -1;
+        stack<int> lefts;
+        for (int i=0; i<s.length(); ++i) {
+            if (s[i]=='(') {
+                lefts.push(i);
+            } else {
+                if (lefts.empty()) {
+                // no matching left
+                    last = i;
+                } else {
+                // find a matching pair
+                    lefts.pop();
+                    if (lefts.empty()) {
+                        maxLen = max(maxLen, i-last);
+                    } else {
+                        maxLen = max(maxLen, i-lefts.top());
+                    }
+                }
+            }
+        }
+        return maxLen;
     }
 };
---
+++ Valid Sudoku.cpp
@@ -0,0 +1,18 @@
+/*
+
+Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+A partially filled sudoku which is valid.
+
+*/
+//2013/6/17 15:27:49
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char> > &board) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+    }
+};

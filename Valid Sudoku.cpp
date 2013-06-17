3620c08d18340b303be858bb86f4541f612e0870
moiyer <zbzhengbin1989@gmail.com>
2013-06-17 16:51:36 +0800

    简单题，不过解法可以借鉴

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -7,12 +7,28 @@ The Sudoku board could be partially filled, where empty cells are filled with th
 A partially filled sudoku which is valid.
 
 */
+
 //2013/6/17 15:27:49
+//只要判断每行、每列、每九宫格无重复数字即可，比较简单,没有动手写
+//这种写法很精炼，只做一次遍历
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char> > &board) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        
+        vector<vector<bool> > rows(9, vector<bool>(9, false));
+        vector<vector<bool> > cols(9, vector<bool>(9, false));
+        vector<vector<bool> > blocks(9, vector<bool>(9, false));
+
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') continue;
+                int c = board[i][j] - '1';
+                if (rows[i][c] || cols[j][c] || blocks[i - i % 3 + j / 3][c])
+                    return false;
+                rows[i][c] = cols[j][c] = blocks[i - i % 3 + j / 3][c] = true;
+            }
+        }
+        return true;
     }
 };

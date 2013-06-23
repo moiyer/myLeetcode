4f3935d4448eb714aac9ae4ecb0afe37981abe5b
moiyer <zbzhengbin1989@gmail.com>
2013-06-23 22:28:53 +0800

    回溯、DFS两种解法，回溯还没有找出问题

--- Combination Sum.cpp
+++ Combination Sum.cpp
@@ -19,28 +19,31 @@ A solution set is:
 */
 
 //2013/6/18 14:30:22
-//典型的递归
+//典型的回溯，递归实现，速度较慢，解法有问题,见注释
+//其实回溯也是对树的一种遍历
 class Solution {
 public:
-	void comIter(std::map<std::vector<int>, bool> &result, std::vector<int> &com, int num, std::vector<int> candidates, int target)
+	void comIter(std::map<std::vector<int>, bool> &result, std::vector<int> &com, int num, std::vector<int>& candidates, int target)
 	{
 		if(target < 0)return;
-		if(0 == num)
+		else if(0 == target)
 		{
-			if(0 == target)
+			if(0 == num)
 			{
 				sort(com.begin(), com.end());
 				result[com] = true;
 			}
 			return;
 		}
+		if(0 == num)return;
 
 		int cSize = candidates.size();
-		for(int i = 0; i < cSize; ++i)
+		for(int i = 0; i < cSize && candidates[i] <= target; ++i)
 		{
 			com.push_back(candidates[i]);
 			comIter(result, com, num - 1, candidates, target - candidates[i]);
-			com.pop_back();
+			com.pop_back();//不能这样，因为可能因为排序，com的序列已经改变，pop_back时不是pop掉43行插入的值
+
 		}
 		return;
 	}
@@ -55,9 +58,94 @@ public:
         for(int i = 1; i <= target; ++i)
         	comIter(tmp, com, i, candidates, target);
 
-        int mSize = tmp.size();
-        for(int i = 0; i < mSize; ++i)
-        	result.push_back(tmp[i]->first);
+        for(auto& x: tmp)//C++11新特性，auto是由编译器自动确定类型
+        	result.push_back(x.first);
         return result;
+    }
+};
+
+
+//2013/6/23 10:13:28
+//修改后的解法，在递归时不排序，而将排序放在最后结果统计时,这时可以不使用map，而是用set
+//还是有问题
+class Solution {
+public:
+    void comIter(std::vector<std::vector<int>> &result, std::vector<int> &com, int num, std::vector<int>& candidates, int target)
+    {
+        if(target < 0)return;
+        else if(0 == target)
+        {
+            if(0 == num)
+            {
+                //sort(com.begin(), com.end());
+                //result[com] = true;修改后不使用map，result参数是一个vector
+                result.push_back(com);
+            }
+            return;
+        }
+        if(0 == num)return;
+
+        int cSize = candidates.size();
+        for(int i = 0; i < cSize && candidates[i] <= target; ++i)
+        {
+            com.push_back(candidates[i]);
+            comIter(result, com, num - 1, candidates, target - candidates[i]);
+            com.pop_back();//修改后可以直接pop_back
+
+        }
+        return;
+    }
+
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        std::vector<std::vector<int> > result;
+        set<std::vector<int> > tmp;
+        std::vector<int> com;
+
+        for(int i = 1; i <= target; ++i)
+            comIter(result, com, i, candidates, target);
+
+        for(auto& x: result)//C++11新特性，auto是由编译器自动确定类型
+            {
+                sort(x.begin(), x.end());
+                tmp.insert(x);
+            }
+
+        result.clear();
+        for(auto& it : tmp)
+            result.push_back(it);
+        return result;
+    }
+};
+
+//2013/6/22 22:42:00
+//这个其实是一棵树的遍历，别人的DFS实现的，之前就考虑好了去重，速度快
+class Solution {
+     void find_subset(vector<int>::iterator first,vector<int>::iterator last, 
+                      vector<int> &comb, int target, vector<vector<int>> &ret){
+         if(target == 0)
+             ret.push_back(comb);
+         if(target < 0)
+             return;
+         vector<int>::iterator it;
+         for(it = first; it != last && *it <= target; it++){
+             comb.push_back(*it);
+             find_subset(it, last, comb, target - *it, ret);
+             comb.pop_back();
+         }
+         
+     }
+public:
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<vector<int>> ret;
+        unique(candidates.begin(), candidates.end());
+        sort(candidates.begin(), candidates.end());
+        vector<int> a;
+        
+        find_subset(candidates.begin(), candidates.end(), a, target, ret);
+        return ret;
     }
 };

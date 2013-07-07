0bd3b523c17e3e74ecdd8f973c0fdcba86cd6821
moiyer <zbzhengbin1989@gmail.com>
2013-07-07 20:52:07 +0800

    还没写

---
+++ Edit Distance.cpp
@@ -0,0 +1,21 @@
+/*
+
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+
+*/
+//2013/7/6 17:48:36
+//二维动态规划
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+    }
+};

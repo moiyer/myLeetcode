146dbffaee960c85d4f13c12e10120e3d13b7374
moiyer <zbzhengbin1989@gmail.com>
2013-07-10 17:29:51 +0800

    还没写

---
+++ Subsets.cpp
@@ -0,0 +1,32 @@
+/*
+
+Given a set of distinct integers, S, return all possible subsets.
+
+Note:
+
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If S = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+*/
+//2013/7/10 16:58:08
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+    }
+};

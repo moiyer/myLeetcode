987f752b7aa79567a07080de3ade785678533a43
moiyer <zbzhengbin1989@gmail.com>
2013-07-21 19:32:02 +0800

    还没写

---
+++ Subsets II.cpp
@@ -0,0 +1,30 @@
+/*
+
+Given a collection of integers that might contain duplicates, S, return all possible subsets.
+
+Note:
+
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If S = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+
+*/
+//2013/7/21 19:27:24
+class Solution {
+public:
+    vector<vector<int> > subsetsWithDup(vector<int> &S) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+    }
+};

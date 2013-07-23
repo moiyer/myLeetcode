d32b1678bb6951a0865a2c35a2063fcfb40617a9
moiyer <zbzhengbin1989@gmail.com>
2013-07-23 20:25:14 +0800

    深度优先，剪枝

--- Subsets II.cpp
+++ Subsets II.cpp
@@ -20,11 +20,47 @@ If S = [1,2,2], a solution is:
 
 */
 //2013/7/21 19:27:24
+//深度优先，做一些剪枝
+//字符在ret中和自己相等的字符中的index应该等于在tp中和自己相等字符的index
+//也可直接使用subsets I，只要在插入结果集之前判断有没有重复
 class Solution {
 public:
+    void subsetDFS(std::vector<int> &tp, std::vector<std::vector<int> > &ret, std::vector<int> &S, int index, int cur_index, int tp_cur_index)
+    {
+      if(cur_index != tp_cur_index)return;
+
+      ret.push_back(tp);
+      if(index == S.size())
+        return;
+
+      int tpp;
+      
+      for(int i = index; i < S.size(); ++i)
+      {
+        // if((((!tp.empty()) && tp.back() != S[i]) || tp.empty()) && S[i] == S[i - 1])
+        //   continue;
+        if(tp.empty() || tp.back() != S[i])tpp = 1;
+        else if(tp.back() == S[i])tpp = tp_cur_index + 1;
+        
+        tp.push_back(S[i]);
+
+        if(i == 0 || S[i - 1] != S[i])subsetDFS(tp, ret, S, index + 1, 1, tpp);
+        else subsetDFS(tp, ret, index + 1, cur_index + 1, tpp);
+        
+        tp.pop_back();
+      }
+    }
+
     vector<vector<int> > subsetsWithDup(vector<int> &S) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        
+        std::vector<int> tp;
+        std::vector<std::vector<int> > ret;
+
+        sort(S.begin(), S.end());
+
+        subsetDFS(tp, ret, S, 0, 1, 1);
+
+        return ret;
     }
 };

39ecd188fa1627a7af082e9cca2cb630526675ad
moiyer <zbzhengbin1989@gmail.com>
2013-08-08 13:38:53 +0800

    别人写的

--- Binary Tree Inorder Traversal.cpp
+++ Binary Tree Inorder Traversal.cpp
@@ -32,6 +32,28 @@ public:
     vector<int> inorderTraversal(TreeNode *root) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        
+        stack<TreeNode *> data;
+        TreeNode *cur;
+        std::vector<int> ret;
+
+        cur = root;
+        while(!cur || !data.empty())
+        {
+        	while(cur != NULL)
+        	{
+        		data.push(cur);
+        		cur = cur->left;
+        	}
+
+        	if(!data.empty())
+        	{
+        		cur = data.top();
+        		data.push(cur);
+        		data.pop();
+        		cur = cur->right;
+        	}
+        }
+
+        return ret;
     }
 };

81fe21d066c388e24845f72778145d3083c155bc
moiyer <zbzhengbin1989@gmail.com>
2013-08-12 13:51:32 +0800

    单链表排序，修改过

--- offen/qsort_Linkedlist.cpp
+++ offen/qsort_Linkedlist.cpp
@@ -1,23 +1,28 @@
 //单链表进行快速排序
 //基本思想是遍历一遍将比head小的节点挖出来建立一个新的链表，然后将这个链表放在head之前
+//双向链表快排也可以用这种思路
 typedef struct Node
 {
 	int val;
 	struct Node *next;
 }LNode;
 
-void partition(LNode** head, LNode** tail)
+void partition(LNode** phead, LNode* tail)
 {
-	if(*head == *tail)return;
+	if(*phead == tail)return;
 
-	LNode *small_head, *small_tail, *big_head, *big_tail, *tp;
-	LNode **cur = &((*head)->next);
-	LNode *end = (*tail)->next;
+	LNode *small_head, *big_head, LNode *small_tail, *big_tail, *tp;
+	LNode **cur = &((*phead)->next);
+	LNode *end = tail->next;
 
-	small_tail = small_head = NULL;
+	small_head = NULL;
+	big_tail = tail;
+	small_head = *phead;
+	small_tail = phead;
+
 	while(*cur != end)
 	{
-		if((*cur)->val >= head->val)
+		if((*cur)->val >= (*phead)->val)
 		{
 			big_tail = *cur;
 			cur = &((*cur)->next);
@@ -32,17 +37,18 @@ void partition(LNode** head, LNode** tail)
 			}else
 			{
 				small_tail->next = tp;
+				small_tail = tp;
 				tp->next = NULL;
 			}
 		}
 	}
-	small_tail->next = *head;
-	big_head = (*head)->next;
+	small_tail->next = *phead;
 
 //注意这一段不能忘记，重新设置head和tail
-	*head = small_head;
-	*tail = big_tail;
+	tp = *phead;
+	*phead = small_head;
+	tail = big_tail;
 
-	partition(head, &small_tail);
-	partition(&big_head, tail);
+	partition(phead, small_tail);
+	partition(&(tp->next), tail);
 }

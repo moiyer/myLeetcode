63b85219d547c496c84047fedaa810dd044fe6d8
moiyer <zbzhengbin1989@gmail.com>
2013-08-13 10:29:34 +0800

    单链表快排，fix了bug

--- offen/qsort_Linkedlist.cpp
+++ offen/qsort_Linkedlist.cpp
@@ -5,20 +5,20 @@ typedef struct Node
 {
 	int val;
 	struct Node *next;
-}LNode;
+}LLNode;
 
-void partition(LNode** phead, LNode* tail)
+void partition(LLNode** phead, LLNode* tail)
 {
 	if(*phead == tail)return;
 
-	LNode *small_head, *big_head, LNode *small_tail, *big_tail, *tp;
-	LNode **cur = &((*phead)->next);
-	LNode *end = tail->next;
+	LLNode *small_head, *big_head, *small_tail, *big_tail, *tp;
+	LLNode **cur = &((*phead)->next);
+	LLNode *end = tail->next;
 
 	small_head = NULL;
-	big_tail = tail;
-	small_head = *phead;
-	small_tail = phead;
+	big_tail = NULL;
+	small_head = NULL;
+	small_tail = NULL;
 
 	while(*cur != end)
 	{
@@ -42,13 +42,53 @@ void partition(LNode** phead, LNode* tail)
 			}
 		}
 	}
-	small_tail->next = *phead;
 
-//注意这一段不能忘记，重新设置head和tail
 	tp = *phead;
-	*phead = small_head;
-	tail = big_tail;
+	if(small_tail != NULL)
+	{
+		small_tail->next = *phead;
+
+		//注意这一段不能忘记，重新设置head和tail
+		*phead = small_head;
+	}else small_tail = *phead;
+
+	if(big_tail != NULL)
+		tail = big_tail;
+	else tail = tp;
 
 	partition(phead, small_tail);
-	partition(&(tp->next), tail);
+	if(tail != tp)partition(&(tp->next), tail);
+}
+
+
+
+//双链表交换两节点
+//需要考虑两个节点相同、相邻、在链表头尾
+//可以想象成两次插入
+typedef struct Node
+{
+	int val;
+	struct Node *left;
+	struct Node *right;
+}LNode;
+
+void insertNode(LNode* cur, LNode *p)//将p插到cur之前
+{
+	;
+}
+
+void swapNode(LNode* a, LNode* b)
+{
+	if(a == NULL || b == NULL || a == b)return;
+	LNode *pa = a;
+	LNode *pb = b;
+	LNode *pbn = b->right;
+	LNode *pbp = b->left;
+	
+	insertNode(a, b);
+	if(pbn != NULL)
+		insertNode(pbn, a);
+	else{
+
+	}
 }

6684d18031bac984cb950a526706c0267f504e1c
moiyer <zbzhengbin1989@gmail.com>
2013-08-27 19:38:08 +0800

    水题

---
+++ Populating Next Right Pointers in Each Node II.cpp
@@ -0,0 +1,95 @@
+/*
+
+Follow up for problem "Populating Next Right Pointers in Each Node".
+
+What if the given tree could be any binary tree? Would your previous solution still work?
+
+Note:
+
+You may only use constant extra space.
+For example,
+Given the following binary tree,
+
+         1
+       /  \
+      2    3
+     / \    \
+    4   5    7
+After calling your function, the tree should look like:
+
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \    \
+    4-> 5 -> 7 -> NULL
+
+*/
+//2013/8/27 17:12:35
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if(root == NULL || (root->left == NULL && root->right == NULL))return;
+        TreeLinkNode *father_head = root;
+        TreeLinkNode *child_head;
+
+        TreeLinkNode *cur;
+        TreeLinkNode *c_pre, *c_cur;
+        while(father_head != NULL)
+        {
+        	cur = father_head;
+        	c_pre = NULL;
+        	child_head = NULL;
+        	while(cur != NULL)
+        	{
+        		if(cur->left != NULL || cur->right != NULL)
+        		{
+        			if(cur->left != NULL && cur->right != NULL)
+        			{
+        				if(child_head == NULL)
+        				{
+        					child_head = cur->left;
+        					cur->left->next = cur->right;
+        					c_pre = cur->right;
+        				}else{
+        					c_pre->next = cur->left;
+        					cur->left->next = cur->right;
+        					c_pre = cur->right;
+        				}
+        			}else if(cur->left != NULL)
+        			{
+        				if(child_head == NULL)
+        				{
+        					child_head = cur->left;
+        					c_pre = cur->left;
+        				}else{
+        					c_pre->next = cur->left;
+        					c_pre = cur->left;
+        				}
+        			}else{
+        				if(child_head == NULL)
+        				{
+        					child_head = cur->right;
+        					c_pre = cur->right;
+        				}else{
+        					c_pre->next = cur->right;
+        					c_pre = cur->right;
+        				}
+        			}
+        		}
+        		cur = cur->next;
+        	}
+        	father_head = child_head;
+        	child_head = NULL;
+        }
+    }
+};
